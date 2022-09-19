aacdd200deac1e6d313d2359d7d534706be3c70f
Soren <sorenisme@gmail.com>
2022-09-19 13:56:09 -0400

    commit tree 0022f8b560accc0f81e200e3f2e41a14b790b16d

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = 'Soren'
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
